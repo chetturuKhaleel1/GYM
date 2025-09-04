901a9168bf8280e9bc72aeffb8805117d18230c5
chetturuKhaleel1 <khaleelchetturu@gamil.com>
2025-09-04 21:05:52 +0530

    Fix react-icons/fa import for Vercel build

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { FaBell } from 'react-icons/fa';
+import { FaBell } from "react-icons/fa/index.esm.js";
+
 import { toast } from 'react-toastify';
 import MemberBills from './member/MemberBills';
 
